82c345f4650cfe4071cd6aa7a0acbc6201236b22
Atreyee Paul <paul.atreyee@gmail.com>
2024-01-19 15:44:14 +0530

    Add project files.

---
+++ WebApplicationProject/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationProject.Models;
+
+namespace WebApplicationProject.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+
+        }
+    }
+}
---
+++ WebApplicationProject/ViewModels/LoginVM.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationProject.ViewModels
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage ="Username is required.")]
+        public string? UserName { get; set; }
+
+        [Required(ErrorMessage ="Password is required.")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+
+    }
+}
